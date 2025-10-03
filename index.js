d89eb34957c051aa4ffa4b0ddbf15c2226538fef
imarunrazput-design <imarunrazput@gmail.com>
2025-10-03 09:32:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,19 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const RAPIDAPI_KEY = process.b1792a54a3msh86938f87bf7bd6fp16bda1jsn9f6d626efbaa
 ;
-const RAPIDAPI_HOST = "instagram-downloader-api.p.rapidapi.com";
+const RAPIDAPI_HOST = "instagram-reels-downloader-api.p.rapidapi.com";
 
 // Root route (homepage test)
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
 });
-
 // Reel download route
 app.get('/download', async (req, res) => {
   const reelUrl = req.query.url;
   if (!reelUrl) return res.status(400).send("URL missing");
-
   try {
     const response = await axios.get(`https://${RAPIDAPI_HOST}/reel`, {
-      params: { url: reelUrl },
+      params: { url: encodeURIComponent(reelUrl) },
       headers: {
         'X-RapidAPI-Key': RAPIDAPI_KEY,
         'X-RapidAPI-Host': RAPIDAPI_HOST
@@ -31,9 +29,11 @@ app.get('/download', async (req, res) => {
     });
 
     res.json(response.data);
-  } catch (err) {
-    console.error("❌ Error:", err.response?.data || err.message);
-    res.status(500).send("Error fetching reel");
+  catch (err) {
+  console.error("❌ Error details:", err.response?.data || err.message);
+  res.status(500).json({ error: err.response?.data || err.message });
+}
+
   }
 });
 
